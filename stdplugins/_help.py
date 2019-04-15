bcbbbe792eed4529cf503ca68f681872393f0e6f
Ravana <47528708+ravana69@users.noreply.github.com>
2019-04-15 15:24:33 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -45,4 +45,4 @@ async def _(event):
     result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
     result = result.stringify()
     logger.info(result)  # pylint:disable=E0602
-    await event.edit("""Telethon UserBot powered by demo""")
+    await event.edit("""This Userbot Is Made With The Help Of https://github.com/ravana69/uniborg""")
