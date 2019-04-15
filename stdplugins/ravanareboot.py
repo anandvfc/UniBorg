14514dfb1e8d9562a1bc01e05ba0b3a78e8bfb04
Anandsudhakaranps <48455768+Anandsudhakaranps@users.noreply.github.com>
2019-04-15 16:30:16 +0530

    update

--- stdplugins/ravanareboot.py
+++ stdplugins/ravanareboot.py
@@ -6,11 +6,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.ravana", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.anand", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("RAVANA"))
+	deq = deque(list("ANAND"))
 	for _ in range(43):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
