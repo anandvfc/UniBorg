9050ea388300607e992a717cfe06f49bca211ac4
Anandsudhakaranps <48455768+Anandsudhakaranps@users.noreply.github.com>
2019-04-15 16:31:35 +0530

    Update vomite.py

--- stdplugins/vomite.py
+++ stdplugins/vomite.py
@@ -6,7 +6,7 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.clock", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.vomite", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
