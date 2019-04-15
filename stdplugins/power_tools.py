605aab32237d8d14bd0056ec6fdc811871f2f83c
Ravana <47528708+ravana69@users.noreply.github.com>
2019-04-15 13:30:05 +0530

    Update power_tools.py

--- stdplugins/power_tools.py
+++ stdplugins/power_tools.py
@@ -15,16 +15,16 @@ import sys
 async def _(event):
     if event.fwd_from:
         return
-    await asyncio.sleep(4)
-    await event.edit("Restarting [░░░] ...\n`.ping` me or `.helpme` to check if I am online")
-    await asyncio.sleep(4)
-    await event.edit("Restarting [█░░] ...\n`.ping` me or `.helpme` to check if I am online")
-    await asyncio.sleep(4)
-    await event.edit("Restarting [██░] ...\n`.ping` me or `.helpme` to check if I am online")
-    await asyncio.sleep(4)
-    await event.edit("Restarting [███]...\n`.ping` me or `.helpme` to check if I am online")
-    await asyncio.sleep(4)
-    await event.edit("Restarted. `.ping` me or `.helpme` to check if I am online")
+    await asyncio.sleep(3)
+    await event.edit("Restarting [🔴] ...\n`.ping` me or `.helpme` to check if I am online")
+    await asyncio.sleep(3)
+    await event.edit("Restarting [⚫] ...\n`.ping` me or `.helpme` to check if I am online")
+    await asyncio.sleep(3)
+    await event.edit("Restarting [🔵] ...\n`.ping` me or `.helpme` to check if I am online")
+    await asyncio.sleep(3)
+    await event.edit("Restarting [✅]...\n`.ping` me or `.helpme` to check if I am online")
+    await asyncio.sleep(3)
+    await event.edit("**♻️Restarted♻️** `.ping` me or `.helpme` to check if I am online")
     await borg.disconnect()
     # https://archive.is/im3rt
     os.execl(sys.executable, sys.executable, *sys.argv)
