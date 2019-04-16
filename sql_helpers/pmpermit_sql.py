32f596ccc108a60a88a62a920d7d5d20515a8a3c
Anandsudhakaranps <48455768+Anandsudhakaranps@users.noreply.github.com>
2019-04-16 20:45:53 +0530

    Update pmpermit_sql.py

--- sql_helpers/pmpermit_sql.py
+++ sql_helpers/pmpermit_sql.py
@@ -1,7 +1,5 @@
+from sqlalchemy import Column, String
 from sql_helpers import SESSION, BASE
-except ImportError:
-    raise Exception("Hello!")
-from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
 
 class PMPermit(BASE):
@@ -42,4 +40,36 @@ def disapprove(chat_id):
 def get_all_approved():
     rem = SESSION.query(PMPermit).all()
     SESSION.close()
-    return rem
+    return rem#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       untitled.py
+#       
+#       Copyright 2019  <>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+#       
+#       
+
+
+
+def main():
+	
+	return 0
+
+if __name__ == '__main__':
+	main()
+
