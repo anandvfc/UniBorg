67a833a51ce3c00f5297e17642417d01fddc07b4
AncestorMoNk <49090708+anandvfc@users.noreply.github.com>
2019-05-06 21:14:02 +0530

    Update ping.py

--- stdplugins/ping.py
+++ stdplugins/ping.py
@@ -1,13 +1,14 @@
 from telethon import events
 from datetime import datetime
+from uniborg.util import admin_cmd
 
 
-@borg.on(events.NewMessage(pattern=r"\.ping", outgoing=True))
+@borg.on(admin_cmd("ping"))
 async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    await event.edit("❗❕Pong❕❗")
+    await event.edit("Pong!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit("❗❕Pong❕❗\n{}".format(ms))
+    await event.edit("Pong!\n{}".format(ms))
